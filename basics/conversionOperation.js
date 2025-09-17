f341e3f6e59401da4f1a509ea071cc1a9bb12d4f
Vikash <vikashverma566@gmail.com>
2025-09-17 12:14:17 +0000

    Operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -25,3 +25,43 @@ let stringNumber =  String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ************************** Operations ***********************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)    // -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power 
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Mohan"
+
+let str3 = str1 + str2
+console.log(str3);  // Output - Hello Mohan
+
+console.log("1" + 2);   // 12
+console.log(1 + "2");   // 12
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 +"2");    // 32
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(true);  // true
+console.log(+true); // 1
+console.log(true+); // error
+console.log(+"");   // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;  // ++gameCounter - 101
+console.log(gameCounter);   // 101
