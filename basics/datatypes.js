8ec1fe993283e13319994e4eac75ff988f6f03fa
Vikash <vikashverma566@gmail.com>
2025-09-17 07:08:54 +0000

    daatatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3 + 3)    //we are using nodejs, not browser
+
+console.log(3 
+    + 3);   // code readability should be high
+
+console.log("Hello World!")
+
+let name = "Mohan"
+let age = 18
+let isLoggedIn = true
+
+
+/* Premitive datatypes */
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+/* Non-Premitive Datatypes*/
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
+
